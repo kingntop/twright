935450d7a0c60d87027ec2ab0d44b34a47ee36b1
limyoungmin <kingntop@naver.com>
2022-07-26 15:19:46 +0900

    11

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,11 @@ import express, {Request, Response, NextFunction} from 'express';
 const app = express();
 
 app.get('/', (request:Request, response:Response, next: NextFunction) => {
-  response.send('hello');
+  const json = {
+    id:'a',
+    pwd:'b'
+  }
+  response.json(json)
 });
 
 app.listen(3000,()=>{
