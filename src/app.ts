1edbbce26ca0a560cababac2bef24bfc665f844f
limyoungmin <kingntop@naver.com>
2022-08-03 13:35:55 +0900

    11

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ const app = express();
 app.get('/:id', async (request:Request, response:Response, next: NextFunction) => {
 
   const delay = (ms: number | undefined)=> new Promise(resolve => setTimeout(resolve, ms))
-  await delay(10000)
+
   const json = {
     id: request.params.id,
     pwd:'b'
